8b574aa8cec6261b80e7c9d196863ff394fdae83
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-09-05 19:18:41 -0300

    dei continuidade ao projeto

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,22 +1,20 @@
 import React from "react";
 import styles from './intro.module.css';
-
+import logo from './whatsapp.png';
 
 function Intro () {
     return (
-        <div class='container-fluid'>
+        <div class={styles.container}>
             
-            <div class='row bg-success'>
-                <div class='col py-5 vh-30'>
-                        <h1>NO</h1>                        
+            <div className={styles.top}>
+                <div className={styles.logoAndName}>
+                    <img className={styles.logo} src={logo} alt="logo" />
+                    <h1 className={styles.name}>Whatsapp Web</h1>
                 </div>
             </div>
-             <div className="row">
-                <div className="col bg-light h-75 w-75 position-absolute">
-                    <h2>eafafa</h2>
-                </div>
-            </div>        
-            
+            <div className={styles.window}>
+
+            </div>
         </div>
     )
 }
