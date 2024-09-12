6cb9ad982cad1f4e651871f1e6d06b8303a7838e
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-09-12 18:46:34 -0300

    dando seguimento na parte de conversas

--- src/components/display/display.js
+++ src/components/display/display.js
@@ -4,7 +4,17 @@ import styles from './display.module.css';
 function Display () {
     return (
         <main className={styles.container}>
-            
+            <section className={styles.personal}>
+                vsdgs
+            </section>
+
+            <section className={styles.contatos}>
+                sgsgs
+            </section>
+
+            <section className={styles.conteudo}>
+                sgsgsg
+            </section>
         </main>
     )
 }
